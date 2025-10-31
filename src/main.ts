ba31bef27a0e3f2fd8b1ffbb1f771b029a0f2ef9
Xander P. <xandwrp@gmail.com>
2025-10-30 21:13:11 -0600

    PixiJS

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,12 @@
-import './style.css'
+import { Application } from "pixi.js";
 
-const canvas = document.getElementById("game") as HTMLCanvasElement;
-const ctx = canvas.getContext("2d")!;
+(async () => {
+  // Create a new application
+  const app = new Application();
 
-// Resize the canvas to match its display size
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+  // Initialize the application
+  await app.init({ background: "#000000ff", resizeTo: window });
 
-function loop() {
-  requestAnimationFrame(loop);
-
-  // Clear frame
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-  // Draw test rect
-  ctx.fillStyle = "red";
-  ctx.fillRect(50, 50, 100, 100);
-
-  // Draw test text
-  ctx.fillStyle = "white";
-  ctx.font = "24px sans-serif";
-  ctx.fillText("linked", 60, 180);
-}
-
-loop();
+  // Append the application canvas to the document body
+  document.getElementById("pixi-container")!.appendChild(app.canvas);
+})();
