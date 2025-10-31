2d9e980a977e6614057d7595de542cc9f52ded8d
Xander P. <xandwrp@gmail.com>
2025-10-31 16:53:22 -0600

    Style

--- app/components/letterSlot.tsx
+++ app/components/letterSlot.tsx
@@ -2,7 +2,7 @@
 
 export default function LetterSlot({ index = 0 }) {
     return (
-        <div className="aspect-square border-2 bg-neutral-600/10 rounded-xl">
+        <div className="aspect-square border-2 bg-black/25 rounded-xl shadow-sm shadow-black/25">
             
         </div>
     );
