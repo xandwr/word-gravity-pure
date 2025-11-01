fad86bb92fcba02915d8979fc7e420527a0af1f1
Xander P. <xandwrp@gmail.com>
2025-10-31 19:48:39 -0600

    Svelte

---
+++ src/lib/game/types.ts
@@ -0,0 +1,12 @@
+// Represents a letter tile instance
+export type TileData = Readonly<{
+    id: string;
+    letter: string;
+    baseScore: number;
+    multiplier: number;
+}>;
+
+// This is a container that can either hold nothing or a TileData object
+export type TileContainer = {
+    heldLetterTile: TileData | null;
+}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+    import { createTile } from "$lib/game/state";
+
+    const test_tile = createTile("A", 1);
+</script>
+
+<div id="letterTile" class="border-2 bg-orange-200 text-black/80 w-full aspect-square flex flex-col items-center">
+    <h class="text-xl">{test_tile.letter}</h>
+    <span class="flex flex-row gap-1">
+        <h class="text-md">{test_tile.baseScore}</h>
+        <p>x</p>
+        <h class="text-md">{test_tile.multiplier}</h>
+    </span>
+</div>
