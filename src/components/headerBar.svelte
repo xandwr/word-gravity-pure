7857e4f58163cf35c6e5ca97c076d9fd17dc5022
Xander P. <xandwrp@gmail.com>
2025-11-02 19:20:12 -0700

    Header bar component

---
+++ src/components/headerBar.svelte
@@ -0,0 +1,8 @@
+<script>
+</script>
+
+<header
+    class="flex flex-col items-center bg-blue-800 text-white p-2 sm:p-3 border-b-2 sm:border-b-4 border-blue-950 shadow-lg"
+>
+    <h1 class="font-bold text-2xl sm:text-3xl md:text-4xl">Word Gravity</h1>
+</header>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,14 +8,11 @@
     import { gameState, HAND_CONFIG } from "$lib/game/state.svelte";
     import PlayerInfoPanel from "../components/playerInfoPanel.svelte";
     import { PLAYER_COLORS } from "$lib/game/constants";
+    import HeaderBar from "../components/headerBar.svelte";
 </script>
 
 <main class="flex flex-col min-h-screen overflow-x-hidden">
-    <header
-        class="flex flex-col items-center bg-blue-800 text-white p-2 sm:p-3 border-b-2 sm:border-b-4 border-blue-950 shadow-lg"
-    >
-        <h1 class="font-bold text-2xl sm:text-3xl md:text-4xl">Word Gravity</h1>
-    </header>
+    <HeaderBar />
 
     {#if gameState.isGameOver}
         <div
